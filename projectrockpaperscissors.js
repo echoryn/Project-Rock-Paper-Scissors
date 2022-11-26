f09aff53d711004a6a9c0ca6e1e597fb1d815ff6
echoryn <echorynnn@gmail.com>
2022-11-26 23:58:37 +0800

    Create an if else statement

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -1,3 +1,14 @@
 function getComputerChoice(randomNum) {
     randomNum = Math.floor((Math.random() * 6) + 1);
+    console.log(randomNum);
+    
+    if (randomNum > 0 <= 4) {
+    console.log ('Blahblah'); 
+    }
+        
+    else {
+    console.log ('Other');
+    }
+    
+console.log (getComputerChoice (randomNum));
 }
