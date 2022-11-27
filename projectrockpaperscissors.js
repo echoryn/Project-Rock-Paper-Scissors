a7a2330bd377df6d7e8276431f4aa4ea0e02f887
echoryn <echorynnn@gmail.com>
2022-11-27 13:39:28 +0800

    Wrap previous code block in a function

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -1,13 +1,17 @@
-let randomNum = (Math.floor(Math.random() * 3) + 1);
+function getComputerChoice {
+    let randomNum = (Math.floor(Math.random() * 3) + 1);
 
-if (randomNum === 1) {
-    console.log(randomNum + ' Rock');
+    if (randomNum === 1) {
+        console.log(randomNum + ' Rock');
+    }
+
+    else if (randomNum === 2) {
+        console.log(randomNum + ' Paper');
+    }
+
+    else if (randomNum === 3) {
+        console.log(randomNum + ' Scissors');
+    }
 }
 
-else if (randomNum === 2) {
-    console.log(randomNum + ' Paper');
-}
-
-else if (randomNum === 3) {
-    console.log(randomNum + ' Scissors');
-}
+getComputerChoice ();
