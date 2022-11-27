6d597b206ec29636018448b6ab2a5720007f5904
echoryn <echorynnn@gmail.com>
2022-11-27 21:15:54 +0800

    Make if else statement into a function declaration

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+/*function getComputerChoice() {
     let randomNum = (Math.floor(Math.random() * 3) + 1);
 
     if (randomNum === 1) {
@@ -15,50 +15,54 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
-
+*/
 
 //Test if if else statement is correct
-let playerSelection = (prompt('Type Rock or Paper or Scissors')).toLowerCase();
-console.log(playerSelection);
+function letsPlay (playerSelection, computerSelection) {
 
-let computerSelection = getComputerChoice();
+    let playerSelection = (prompt('Type Rock or Paper or Scissors')).toLowerCase();
+    console.log(playerSelection);
 
-if (playerSelection === 'rock' && computerSelection === 'paper') {
-console.log ('You Lose! Paper beats Rock!');
-}
+    let computerSelection = getComputerChoice();
 
-else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-console.log ('You Win!  Rock beats Scissors!');
-}
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+    console.log ('You Lose! Paper beats Rock!');
+    }
 
-else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-console.log ('You Win!  Scissors beats Paper!');
-}
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    console.log ('You Win!  Rock beats Scissors!');
+    }
 
-else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-console.log ('You Lose! Rock beats Scissors!');
-}
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    console.log ('You Win!  Scissors beats Paper!');
+    }
 
-else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-console.log ('You Lose! Scissors beats Paper!');
-}
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    console.log ('You Lose! Rock beats Scissors!');
+    }
 
-else if (playerSelection === 'paper' && computerSelection === 'rock') {
-console.log ('You Win! Paper beats Rock!');
-}
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    console.log ('You Lose! Scissors beats Paper!');
+    }
 
-else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    console.log ('You Win! Paper beats Rock!');
+    }
+
+    else if (playerSelection === 'paper' && computerSelection === 'paper') {
     console.log ("It's a Tie");
-}
+    }
 
-else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    else if (playerSelection === 'rock' && computerSelection === 'rock') {
     console.log ("It's a Tie");
-}
+    }
 
-else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+    else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
     console.log ("It's a Tie");
-}
+    }
 
-else {
+    else {
     console.log ('Did you type that correctly? Please try again');
+    }
 }
+
