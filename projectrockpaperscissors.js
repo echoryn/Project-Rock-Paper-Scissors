11a268d766843255a9ce8811430b6a26d5af6a69
echoryn <echorynnn@gmail.com>
2022-11-27 21:30:55 +0800

    Test if function works. Results in error getComputerChoice not defined

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -65,6 +65,7 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
+
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
