6f1322453ccd97f8b1f670934453cde8ec00b85d
echoryn <echorynnn@gmail.com>
2022-11-27 21:13:32 +0800

    Revert computer's variable to the previous one

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -23,39 +23,39 @@ console.log(playerSelection);
 
 let computerSelection = getComputerChoice();
 
-if (playerSelection === 'rock' && getComputerChoice() === 'paper') {
+if (playerSelection === 'rock' && computerSelection === 'paper') {
 console.log ('You Lose! Paper beats Rock!');
 }
 
-else if (playerSelection === 'rock' && getComputerChoice() === 'scissors') {
+else if (playerSelection === 'rock' && computerSelection === 'scissors') {
 console.log ('You Win!  Rock beats Scissors!');
 }
 
-else if (playerSelection === 'scissors' && getComputerChoice() === 'paper') {
+else if (playerSelection === 'scissors' && computerSelection === 'paper') {
 console.log ('You Win!  Scissors beats Paper!');
 }
 
-else if (playerSelection === 'scissors' && getComputerChoice() === 'rock') {
+else if (playerSelection === 'scissors' && computerSelection === 'rock') {
 console.log ('You Lose! Rock beats Scissors!');
 }
 
-else if (playerSelection === 'paper' && getComputerChoice() === 'scissors') {
+else if (playerSelection === 'paper' && computerSelection === 'scissors') {
 console.log ('You Lose! Scissors beats Paper!');
 }
 
-else if (playerSelection === 'paper' && getComputerChoice() === 'rock') {
+else if (playerSelection === 'paper' && computerSelection === 'rock') {
 console.log ('You Win! Paper beats Rock!');
 }
 
-else if (playerSelection === 'paper' && getComputerChoice() === 'paper') {
+else if (playerSelection === 'paper' && computerSelection === 'paper') {
     console.log ("It's a Tie");
 }
 
-else if (playerSelection === 'rock' && getComputerChoice() === 'rock') {
+else if (playerSelection === 'rock' && computerSelection === 'rock') {
     console.log ("It's a Tie");
 }
 
-else if (playerSelection === 'scissors' && getComputerChoice() === 'scissors') {
+else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
     console.log ("It's a Tie");
 }
 
