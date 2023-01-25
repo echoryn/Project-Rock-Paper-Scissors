85d196af23bdc3b180e6a0c727a4b9f4a8b1fa2e
echoryn <echorynnn@gmail.com>
2023-01-25 22:57:18 +0800

    Create if else statement to display rock, paper, scissors

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -1,1 +1,13 @@
 const randomNumber = Math.floor(Math.random() * 3) + 1;
+if (randomNumber === 1) {
+    console.log ( 'Rock' );
+}
+else if (randomNumber === 2) {
+    console.log ( 'Paper' );
+}
+else if (randomNumber === 3) {
+    console.log ( 'Scissors' );
+}
+else {
+    console.log ( 'Try again' );
+}
