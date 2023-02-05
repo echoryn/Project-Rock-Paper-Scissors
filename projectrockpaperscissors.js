4c3c24a366478d62fe0d140e5f9b228c420a0b4a
echoryn <echorynnn@gmail.com>
2023-02-05 22:20:46 +0800

    Ask console log to show player and computer's selection, and the winner

--- projectrockpaperscissors.js
+++ projectrockpaperscissors.js
@@ -45,7 +45,7 @@ function playRound(player, computer) {
 
  const player = "rock";
  const computer = getComputerChoice();
- console.log(playRound(player, computer));
+ console.log(player, computer, playRound (player, computer));
 
  /*
  let computer = getComputerChoice();
